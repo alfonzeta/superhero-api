cd673da99b71557680dfa1a0d8d2242fde09b021
alfonzeta <alfonso.za94@gmail.com>
2025-02-02 23:42:27 +0100

    feat: update Fastify version and add Swagger documentation for Superhero API

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,41 @@ import { PrismaClient } from "@prisma/client";
 import fastify from "fastify";
 import { container } from "./config/di";
 import registerRoutes from "./config/routes";
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUi from "@fastify/swagger-ui";
 
 const server = fastify({
   logger: true,
 });
-const prisma = new PrismaClient({
-  datasources: {
-    db: {
-      url: process.env.DATABASE_URL,
+
+server.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: "Superhero API",
+      description: "Superhero API Documentation",
+      version: "0.0.0",
     },
+    components: {
+      securitySchemes: {
+        BearerAuth: {
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT",
+        },
+      },
+    },
+  },
+});
+
+server.register(fastifySwaggerUi, {
+  routePrefix: "/documentation",
+  uiConfig: {
+    docExpansion: "full",
+    deepLinking: false,
+  },
+  staticCSP: true,
+  transformSpecification: (swaggerObject, request, reply) => {
+    return swaggerObject;
   },
 });
 
@@ -33,11 +59,11 @@ server.listen({ port: 8080, host: "0.0.0.0" }, async (err, address) => {
 
 // Gracefully disconnect Prisma when the server is shutting down
 process.on("SIGINT", async () => {
-  await prisma.$disconnect();
+  await container.prisma.$disconnect();
   process.exit(0);
 });
 
 process.on("SIGTERM", async () => {
-  await prisma.$disconnect();
+  await container.prisma.$disconnect();
   process.exit(0);
 });
